02336081223605a21a8263bc00cd51a69978d64c
Roshan <shahroshan0720@gmail.com>
2023-11-06 18:58:41 +0530

    logout btn setup

--- Client/main.js
+++ Client/main.js
@@ -1,5 +1,21 @@
 const loginBtn = document.querySelector('.login-btn');
+const dynSlot = document.querySelector('.dyn-slot');
 
 loginBtn.addEventListener('click',()=>{
     location.href = '/login/index.html';
-})
+});
+
+if(localStorage.getItem('token')){
+    dynSlot.innerHTML = `
+        <button class="btn logout-btn" style="background:red;">Logout</button>`;
+    
+    const logoutBtn = document.querySelector('.logout-btn');
+    logoutBtn.addEventListener('click',()=>{
+        localStorage.clear();
+        location.reload();
+    })
+}
+
+
+
+
