927b6d539162b5ce69ef8b86066ba6cd57b51dab
Roshan <shahroshan0720@gmail.com>
2023-11-06 17:37:27 +0530

    login setup

---
+++ Client/login/login.js
@@ -0,0 +1,30 @@
+const loginBtn = document.getElementById('login-btn');
+const inputEmail = document.getElementById('InputEmail');
+const inputPassword = document.getElementById('InputPassword');
+
+loginBtn.addEventListener('click', async (e) => {
+    e.preventDefault();
+    const email = inputEmail.value, password = inputPassword.value;
+    const body = {
+        email,
+        password
+    }
+
+    try {
+        const res = await fetch('http://localhost:3245/api/v1/auth/login', {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const resData = await res.json();
+        alert(resData.message);
+        if(resData.success){
+            localStorage.setItem('token',resData.token);
+            location.href = '/index.html';
+        }
+    } catch (error) {
+        alert('Something went wrong, Try Again later');
+    }
+});
