cf0ced893304655f7b8edb58902863f0d63c4834
Roshan <shahroshan0720@gmail.com>
2023-11-09 22:28:48 +0530

    git doctor profile updated

--- Client/profile/profile.js
+++ Client/profile/profile.js
@@ -2,72 +2,228 @@ const profileContainer = document.querySelector('.profile-container');
 const detailsContainer = document.querySelector('.details-container');
 
 const start = async () => {
-    let endpoint;
 
-    if (localStorage.getItem('role') === 'patient')
-        endpoint = 'api/v1/user/profile/me';
-    else
-        endpoint = 'api/v1/doctor/profile/me';
+    if (localStorage.getItem('role') === 'patient') {
 
-    try {
-        const res = await fetch(`http://localhost:3245/${endpoint}`, {
-            headers: {
-                Authorization: `Bearer ${localStorage.getItem('token')}`
+        try {
+            const res = await fetch(`http://localhost:3245/api/v1/user/profile/me`, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`
+                }
+            });
+
+            const resData = await res.json();
+            if (resData.success) {
+                console.log(resData);
+
+                profileContainer.innerHTML = `
+                    <p>${resData.data.name}</p>
+                    <p>${resData.data.email}</p>
+                    <div class="btn-container">
+                        <button class="logout-btn">Logout</button>
+                        <button class="delete-btn">Delete Account</button>
+                    </div>`;
+
+                detailsContainer.innerHTML = `
+                    <div>
+                        <button class="booking-btn"> My Appointments</button>
+                        <button class="profile-btn"> Edit Profile</button>
+                    </div>`;
+
+
+                const logoutBtn = document.querySelector('.logout-btn');
+                const deleteBtn = document.querySelector('.delete-btn');
+                const showBooking = document.querySelector('.booking-btn');
+                const profile = document.querySelector('.profile-btn');
+
+                logoutBtn.addEventListener('click', () => {
+                    localStorage.clear();
+                    location.href = '/index.html';
+                });
+
+                deleteBtn.addEventListener('click', async () => {
+                    try {
+                        const deleteRes = await fetch(`http://localhost:3245/api/v1/user/${resData.data._id}`, {
+                            method: 'DELETE',
+                            headers: {
+                                'Authorization': `Bearer ${localStorage.getItem('token')}`
+                            }
+                        });
+
+                        const deleteResdata = await deleteRes.json();
+                        alert(deleteResdata.message);
+                        localStorage.clear();
+                        location.href = '/index.html';
+                    } catch (error) {
+                        alert('Something went wrong, Try again');
+                    }
+                });
+
+                profile.addEventListener('click', showProfile(resData));
             }
-        });
+        } catch (error) {
+            alert('Something went Wrong, Try again later');
+        }
+    }
+    else {
+        console.log('hi');
+        try {
+            const res = await fetch(`http://localhost:3245/api/v1/doctor/profile/me`, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`
+                }
+            });
 
-        const resData = await res.json();
-        if (resData.success) {
-            console.log(resData);
-
-            profileContainer.innerHTML = `
-                <p>${resData.data.name}</p>
-                <p>${resData.data.email}</p>
+            const resData = await res.json();
+            if (resData.success) {
+                console.log(resData);
+                profileContainer.innerHTML = `
+                <ul class="option-list">
+                    <li>Overview</li>
+                    <li>Appointments</li>
+                    <li>Profile</li>
+                </ul>
                 <div class="btn-container">
                     <button class="logout-btn">Logout</button>
                     <button class="delete-btn">Delete Account</button>
                 </div>`;
 
-            detailsContainer.innerHTML = `
-                <div>
-                    <button class="booking-btn"> My Appointments</button>
-                    <button class="profile-btn"> Edit Profile</button>
-                </div>`;
+                const optionList = document.querySelector('.option-list');
 
+                optionList.addEventListener('click', (e) => {
+                    console.log(e.target.innerText);
+                    if (resData.data.isApproved == 'pending') {
+                        detailsContainer.innerHTML = `
+                        <div class="alert alert-info alert-dismissible" role="alert">
+                            <div>To get approval please complete your profile.We'll review manually and approve within 3 days.</div>
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>`;
+                    }
+                    const divEle = document.createElement('div');
+                    divEle.className = 'inner-container';
 
-            const logoutBtn = document.querySelector('.logout-btn');
-            const deleteBtn = document.querySelector('.delete-btn');
-            const showBooking = document.querySelector('.booking-btn');
-            const profile = document.querySelector('.profile-btn');
+                    if (e.target.innerText == 'Overview') {
+                        divEle.innerHTML = `
+                            <h4>About of ${resData.data.name}</h4>
+                            <p>${resData.data.about ? resData.data.about : 'Not available'}</p>
+                            <div class= "edu-container">
+                                <h4>Education</h4>
+                            </div>
+                            <div class= "exp-container">
+                                <h4>Experience</h4>
+                            </div>`;
 
-            logoutBtn.addEventListener('click', () => {
-                localStorage.clear();
-                location.href = '/index.html';
-            });
+                        detailsContainer.appendChild(divEle);
 
-            deleteBtn.addEventListener('click', async() => {
-                try{
-                    const deleteRes = await fetch(`http://localhost:3245/api/v1/user/${resData.data._id}`,{
-                        method: 'DELETE',
-                        headers:{
-                            'Authorization': `Bearer ${localStorage.getItem('token')}`
+                        const eduContainer = document.querySelector('.edu-container');
+                        const expContainer = document.querySelector('.exp-container');
+
+                        if (resData.data.qualifications.length) {
+                            const eduContent = (resData.data.qualifications).forEach(qual => {
+                                return `<tr>
+                                        <td>
+                                            ${qual}
+                                        </td>
+                                    </tr>`
+                            }).join('');
+
+                            const eduTable = document.createElement('table');
+                            eduTable.className = 'edu-table';
+
+                            eduTable.innerHTML = eduContent;
+                            eduContainer.appendChild(eduTable);
+                        } else {
+                            const pTag = document.createElement('p');
+                            pTag.innerText = 'Not Available';
+                            eduContainer.appendChild(pTag);
                         }
-                    });
-        
-                    const deleteResdata = await deleteRes.json();
-                    alert(deleteResdata.message);
-                    localStorage.clear();
-                    location.href = '/index.html';
-                }catch(error){
-                    alert('Something went wrong, Try again');
-                }
-            });
 
-            profile.addEventListener('click', showProfile(resData));
+                        if (resData.data.experiences.length) {
+                            const expContent = (resData.data.experiences).forEach(exp => {
+                                return `
+                            <div class="card" style="width: 18rem;">
+                                <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                          </div>`
+                            }).join('');
+
+                            const expBlock = document.createElement('div');
+                            expBlock.className = 'exp-block';
+
+                            expBlock.innerHTML = expContent;
+
+                            expContainer.appendChild(expBlock);
+
+                        } else {
+                            const pTag = document.createElement('p');
+                            pTag.innerText = 'Not Available';
+                            expContainer.appendChild(pTag);
+                        }
+
+                    } else if (e.target.innerText == 'Appointments') {
+
+                    } else if (e.target.innerText == 'Profile') {
+                        divEle.innerHTML = `
+                        <h1>Profile Details</h1>
+                        <form class="row g-3">
+                            <div class="col-md-12">
+                                <label for="Username" class="form-label">Name</label>
+                                <input type="text" class="form-control" id="InputUsername" value=${resData.data.name}>
+                            </div>
+                            <div class="col-md-12">
+                                <label for="Email" class="form-label">Email</label>
+                                <input type="email" class="form-control" id="InputEmail" value=${resData.data.email} disabled>
+                            </div>
+  
+                            <div class="col-12">
+                                <label for="Phone" class="form-label">Phone</label>
+                                <input type="text" class="form-control" placeholder="Enter your Phone number" id="InputPhone" value=${resData.data.phone ? resData.data.phone : ''}>
+                            </div>
+                            <div class="col-12">
+                                <label for="inputAddress2" class="form-label">Bio</label>
+                                <textarea class="form-control" placeholder="Enter your Bio" name="Bio" id="InputBio" rows="3">${resData.data.bio ? resData.data.bio : ''}</textarea>
+                            </div>
+                            <div class="col-md-4">
+                                <label for="gender" class="form-label">Gender</label>
+                                <select id="InputGender" class="form-select">
+                                    <option value="male" ${resData.data.gender === 'male' ? 'selected' : ''}>Male</option>
+                                    <option value="female" ${resData.data.gender === 'female' ? 'selected' : ''}>Female</option>
+                                    <option value="other" ${resData.data.gender === 'other' ? 'selected' : ''}>Other</option>
+                                </select>
+                            </div>
+                            <div class="col-md-4">
+                                <label for="Specialization" class="form-label">Specialization</label>
+                                <select id="InputSpecialization" class="form-select">
+                                    <option selected>Choose...</option>
+                                    <option>...</option>
+                                </select>
+                            </div>
+                            <div class="col-md-2">
+                                <label for="Price" class="form-label">Fee</label>
+                                <input type="number" placeholder="Enter Fee" id="InputPrice" value=${resData.data.ticketPrice ? resData.data.ticketPrice : ''}>
+                            </div>
+                            <div id="educationFields">
+                            <div class="education">
+                                <input type="date" placeholder="Starting Date">
+                                <input type="date" placeholder="Ending Date">
+                                <input type="text" placeholder="Degree">
+                                <input type="text" placeholder="University">
+                            </div>
+                            <button onclick="addEducationField()">Add More Education Field</button>
+                            </div>`;
+                    
+                        detailsContainer.appendChild(divEle);
+                    }
+                })
+            }
+        } catch (error) {
+
         }
-    } catch (error) {
-        alert('Something went Wrong, Try again later');
     }
+
 }
 
 start();
@@ -84,9 +240,9 @@ const showProfile = (resData) => {
         <div style="margin-top: 10px;">
             <label for="gender"><strong>Gender: </strong></label>
                 <select name="gender" id="InputGender">
-                    <option value="male" ${resData.data.gender === 'male'? 'selected': ''}>Male</option>
-                    <option value="female" ${resData.data.gender === 'female'? 'selected': ''}>Female</option>
-                    <option value="other" ${resData.data.gender === 'other'? 'selected': ''}>Other</option>
+                    <option value="male" ${resData.data.gender === 'male' ? 'selected' : ''}>Male</option>
+                    <option value="female" ${resData.data.gender === 'female' ? 'selected' : ''}>Female</option>
+                    <option value="other" ${resData.data.gender === 'other' ? 'selected' : ''}>Other</option>
                 </select>
         </div>
         <button class="update-btn"> Update Profile</button>`;
@@ -98,26 +254,26 @@ const showProfile = (resData) => {
     const inputBlood = document.getElementById('InputBloodGroup');
     const inputGender = document.getElementById('InputGender');
 
-    updateBtn.addEventListener('click',async()=>{
+    updateBtn.addEventListener('click', async () => {
         const name = inputName.value, phone = inputPhone.value, blood = inputBlood.value,
-        gender = inputGender.value;
+            gender = inputGender.value;
 
         const body = {
             name,
             gender
         }
 
-        if(phone !== '')
+        if (phone !== '')
             body.phone = phone;
-        
-        if(blood !== '')
+
+        if (blood !== '')
             body.bloodType = blood;
-        
-        try{
-            const updateRes = await fetch(`http://localhost:3245/api/v1/user/${resData.data._id}`,{
+
+        try {
+            const updateRes = await fetch(`http://localhost:3245/api/v1/user/${resData.data._id}`, {
                 method: 'PUT',
                 body: JSON.stringify(body),
-                headers:{
+                headers: {
                     'Content-Type': 'application/json',
                     'Authorization': `Bearer ${localStorage.getItem('token')}`
                 }
@@ -125,7 +281,7 @@ const showProfile = (resData) => {
 
             const updateResdata = await updateRes.json();
             alert(updateResdata.message);
-        }catch(error){
+        } catch (error) {
             alert('Something went wrong, Try again');
         }
     })
